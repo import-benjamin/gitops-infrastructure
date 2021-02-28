5ea1c89a964d3e2f98e93ce3a8d376a2e80bf07c
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2021-02-28 18:46:32 +0100

    Got argo-cd installation with kustomize working
    Using argo-cd as a submodule to properly include install.yaml

--- tools/install-pristine-argocd.sh
+++ tools/install-pristine-argocd.sh
@@ -3,4 +3,4 @@ kubectl create namespace argocd
 # Apply argocd manifest from given stable branch
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # Create service to forward port
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+# kubectl port-forward svc/argocd-server -n argocd 8080:443
